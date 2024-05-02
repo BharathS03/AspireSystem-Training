ff734a4f97c23b50612597a1d12d6e4ac0773f89
BharathS03 <149763876+BharathS03@users.noreply.github.com>
2024-05-02 21:10:11 +0530

    Add files via upload

---
+++ JS Practice/array.js
@@ -0,0 +1,26 @@
+// Title : array & object
+// Author : Bharath S
+// Created Date : 21-03-2024
+// Modified Date : 02-04-2024
+// Revieweb By :
+// Reviewed Date :
+
+
+//array concept
+let employee=['employee_name','employee_id','Phone'];
+console.log(employee);
+console.log(employee[0]);
+console.log(employee[1]);
+console.log(employee.length);
+
+let student = new Array('name','id','phone','e-mail')
+console.log("student array:",student);
+
+
+// object concept
+let employee_details={
+    "employee_name":"Bharath",
+    "employee_id":101,
+    "Phone":6369176255};
+    console.log(employee_details);
+    console.table(employee_details);
